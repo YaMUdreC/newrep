637443276c8b2e77ea0846b4c05f5b7fc7dff19b
YaMUdreC <yarbudkov@gmail.com>
2024-05-13 15:42:30 +0300

    finishing that one

--- FPW_html/js/sript.js
+++ FPW_html/js/sript.js
@@ -1,1 +1,12 @@
 function doFunction() { alert("HELLO");}
+
+const button = document.querySelector('.button');
+const panel = document.querySelector('.panel');
+
+button.addEventListener('click', () => {
+  if (panel.style.display === 'none') {
+    panel.style.display = 'block';
+  } else {
+    panel.style.display = 'none';
+  }
+});
